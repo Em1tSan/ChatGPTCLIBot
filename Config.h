0021a17acefddc734404404bb2778564022e6567
Roman S <70670181+Em1tSan@users.noreply.github.com>
2023-12-11 13:07:08 +0800

    Add files via upload

---
+++ Config.h
@@ -0,0 +1,61 @@
+//
+// Created by v2ray on 2023/5/10.
+//
+
+#ifndef GPT3BOT_CONFIG_H
+#define GPT3BOT_CONFIG_H
+
+#include "interface/model/Completion.h"
+#include "interface/log/LogMsg.h"
+
+namespace config {
+
+    class Config {
+    public:
+        const std::filesystem::path config_path;
+        std::vector<std::string> input_history;
+        const std::string default_initial_prompt_filename = "Default";
+        const std::string f_initial = "initial";
+        const std::string f_saved = "saved";
+        const std::string f_documentQA = "documentQA";
+        std::string api_base_url = "https://api.openai.com";
+    private:
+        std::string model = "gpt-3.5-turbo";
+        bool is_new_api_ = true;
+    public:
+        float temperature = 1;
+        int max_tokens = 500;
+        float top_p = 1;
+        float frequency_penalty = 0;
+        float presence_penalty = 0;
+        std::vector<std::pair<std::string, float>> logit_bias;
+        std::string initial_prompt = "You are an AI chat bot named Sapphire\n"
+                                     "You are friendly and intelligent\n"
+                                     "Your backend is OpenAI's ChatGPT API\n";
+        std::shared_ptr<chat::ExchangeHistory> chat_history = std::make_shared<chat::ExchangeHistory>();
+        unsigned int max_display_length = 100;
+        unsigned int max_short_memory_length = 4;
+        unsigned int max_reference_length = 4;
+        std::shared_ptr<std::vector<std::shared_ptr<doc::Document>>> documents;
+        bool search_response = true;
+        bool space_between_exchanges = false;
+        bool debug_reference = false;
+
+        Config();
+        explicit Config(std::filesystem::path config_path);
+        virtual ~Config();
+
+        [[nodiscard]] chat::Completion to_completion() const;
+
+        void load_config(const std::function<void(const Log::LogMsg<std::filesystem::path>& msg)>& log_callback = [](const auto&){});
+        void save_config(const std::function<void(const Log::LogMsg<std::filesystem::path>& msg)>& log_callback = [](const auto&){});
+        void load_documents(const std::string& filename,
+                            const std::function<void(const Log::LogMsg<std::filesystem::path>& msg)>& log_callback = [](const auto&){});
+
+        [[maybe_unused]] [[nodiscard]] std::string get_model() const;
+        [[maybe_unused]] void set_model(const std::string& model_);
+        [[nodiscard]] bool is_new_api() const;
+    };
+} // config
+
+#endif //GPT3BOT_CONFIG_H
---
+++ GPTMain.h
@@ -0,0 +1,42 @@
+//
+// Created by v2ray on 2023/2/18.
+//
+
+#ifndef GPT3BOT_GPTMAIN_H
+#define GPT3BOT_GPTMAIN_H
+
+#include "interface/network/Network.h"
+#include "Config.h"
+#include "Command.h"
+#include "FineTuneHelper.h"
+#include "Translator.h"
+#include "csignal"
+
+#define PATH_S(path) Term::color_fg(125, 225, 255) + "\"" + (path).string() + "\""
+#define ENTER Term::color_fg(70, 200, 255) + "Enter" + Term::color_fg(Term::Color::Name::Default)
+#define GOLDEN_TEXT(n) Term::color_fg(255, 200, 0) + (n) + Term::color_fg(Term::Color::Name::Default)
+
+namespace GPT {
+    using Color = Term::Color;
+
+    inline const std::string f_suffix = ".txt";
+    inline const std::string json_suffix = ".json";
+    inline const std::string me_id = "User";
+    inline const std::string bot_id = "Bot";
+
+    void pre_settings();
+    void start_loop();
+    void print_prompt();
+    void print_enter_next_cycle();
+    void clear_console();
+    bool create_folders(const std::vector<std::string>& folders);
+    bool p_default_prompt();
+    bool p_load_prompt(std::string filename);
+    bool p_load_saved(std::string filename);
+    bool p_save_chat(std::string name);
+    bool p_create_docQA();
+    bool p_check_set_api_key();
+    void p_on_invalid_key();
+}
+
+#endif //GPT3BOT_GPTMAIN_H
